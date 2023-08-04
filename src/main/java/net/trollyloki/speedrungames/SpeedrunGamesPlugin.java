fbd9f9460f2644f382e92bed6da60f2dbfbcff5c
TrollyLoki <trollyloki@gmail.com>
2023-08-04 13:14:14 -0400

    Initial commit

---
+++ src/main/java/net/trollyloki/speedrungames/SpeedrunGamesPlugin.java
@@ -0,0 +1,12 @@
+package net.trollyloki.speedrungames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpeedrunGamesPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
