92359a991e91042589739ea0ba46d3d81cd01da5
TrollyLoki <trollyloki@gmail.com>
2023-08-04 22:44:28 -0400

    Add configuration support

--- src/main/java/net/trollyloki/speedrungames/SpeedrunGamesPlugin.java
+++ src/main/java/net/trollyloki/speedrungames/SpeedrunGamesPlugin.java
@@ -1,8 +1,13 @@
 package net.trollyloki.speedrungames;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public class SpeedrunGamesPlugin extends JavaPlugin {
+
+    public static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
 
     private static SpeedrunGamesPlugin instance;
 
@@ -11,6 +16,8 @@ public class SpeedrunGamesPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+
+        saveDefaultConfig();
 
         goalListener = new GoalListener();
         getServer().getPluginManager().registerEvents(goalListener, this);
@@ -31,6 +38,27 @@ public class SpeedrunGamesPlugin extends JavaPlugin {
 
     public static GoalListener getGoalListener() {
         return goalListener;
+    }
+
+    /**
+     * Gets a message from the configuration, formatted using MiniMessage.
+     *
+     * @param path     path of the message
+     * @param fallback fallback message
+     * @return message or fallback message if the path does not exist
+     */
+    public @NotNull Component getConfigMessage(@NotNull String path, @NotNull Component fallback) {
+        return MINI_MESSAGE.deserializeOr(getConfig().getString(path), fallback);
+    }
+
+    /**
+     * Gets a message from the configuration, formatted using MiniMessage.
+     *
+     * @param path path of the message
+     * @return message or {@code null} if the path does not exist
+     */
+    public Component getConfigMessage(@NotNull String path) {
+        return MINI_MESSAGE.deserializeOrNull(getConfig().getString(path));
     }
 
 }
