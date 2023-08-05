355bb5b4008325a046f7eabfaf535750e9dcf318
TrollyLoki <trollyloki@gmail.com>
2023-08-04 22:25:30 -0400

    Add goal listener

---
+++ src/main/java/net/trollyloki/speedrungames/Goal.java
@@ -0,0 +1,62 @@
+package net.trollyloki.speedrungames;
+
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityPortalEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerPortalEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Defines a team's goal in a speedrun.
+ */
+public interface Goal {
+
+    /**
+     * Checks if the goal has been reached.
+     * 
+     * @return {@code true} if the team has completed their goal, otherwise {@code false}
+     */
+    boolean isComplete();
+
+    default void onEntityDamage(@NotNull EntityDamageEvent event) {
+        // to be overridden
+    }
+
+    default void onEntityDamageByEntity(@NotNull EntityDamageByEntityEvent event) {
+        // to be overridden
+    }
+
+    default void onEntityDeath(@NotNull EntityDeathEvent event) {
+        // to be overridden
+    }
+
+    default void onPlayerDeath(@NotNull PlayerDeathEvent event) {
+        // to be overridden
+    }
+
+    default void onPlayerRespawn(@NotNull PlayerRespawnEvent event) {
+        // to be overridden
+    }
+
+    default void onPlayerPortal(@NotNull PlayerPortalEvent event) {
+        // to be overridden
+    }
+
+    default void onEntityPortal(@NotNull EntityPortalEvent event) {
+        // to be overridden
+    }
+
+    default void onPlayerJoin(@NotNull PlayerJoinEvent event) {
+        // to be overridden
+    }
+
+    default void onPlayerAdvancementDone(@NotNull PlayerAdvancementDoneEvent event) {
+        // to be overridden
+    }
+
+}
--- src/main/java/net/trollyloki/speedrungames/SpeedrunGamesPlugin.java
+++ src/main/java/net/trollyloki/speedrungames/SpeedrunGamesPlugin.java
@@ -4,9 +4,33 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpeedrunGamesPlugin extends JavaPlugin {
 
+    private static SpeedrunGamesPlugin instance;
+
+    private static GoalListener goalListener;
+
     @Override
     public void onEnable() {
+        instance = this;
 
+        goalListener = new GoalListener();
+        getServer().getPluginManager().registerEvents(goalListener, this);
+
+    }
+
+    @Override
+    public void onDisable() {
+        instance = null;
+
+        goalListener = null;
+
+    }
+
+    public static SpeedrunGamesPlugin getInstance() {
+        return instance;
+    }
+
+    public static GoalListener getGoalListener() {
+        return goalListener;
     }
 
 }
